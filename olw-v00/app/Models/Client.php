92cceb2b706b59d1b5d49364f7c83023346fb585
Weverton Campos <wevertoncamposdev@gmail.com>
2023-02-07 22:30:53 -0300

    update

---
+++ olw-v00/app/Models/Client.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'address_id',
+        'user_id'
+    ];
+
+    public function address(): HasOne
+    {
+        return $this->hasOne(Address::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ olw-v00/app/Models/Saller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Faker\Provider\ar_EG\Company;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Saller extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_id',
+        'user_id'
+    ];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
