21b17333978f63a7b17ac07f19a3c82fadbea14a
Weverton Campos <wevertoncamposdev@gmail.com>
2023-02-12 02:58:53 -0300

    update

--- olw-v00/app/Http/Livewire/Table.php
+++ olw-v00/app/Http/Livewire/Table.php
@@ -3,11 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Table extends Component
 {
+    use WithPagination;
+
+    public string $resource;
+    public array $columns;
+    public string $edit;
+    public string $delete;
+
     public function render()
     {
-        return view('livewire.table');
+        return view('livewire.table', [
+            'items' => app("App\Models\\" . $this->resource)->paginate(10)
+        ]);
     }
 }
