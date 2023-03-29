4e717bff3fcd6c776de9f02fdbf957844f0fdaae
Weverton Campos <wevertoncamposdev@gmail.com>
2023-03-29 13:56:09 -0300

    update

--- olw-v00/routes/web.php
+++ olw-v00/routes/web.php
@@ -28,7 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
     Route::resource('/clients', ClientController::class);
     Route::get('/sales', [SaleController::class, 'index']);
 });
